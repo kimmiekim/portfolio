9c8ef3a79618221908f0737f03781378d9226069
Erin Kim <kim.erin.kim@gmail.com>
2016-06-26 13:58:27 -0400

    fix greeting to not push down elements on slide down, and restyled project section

--- app/assets/javascripts/greeting.js
+++ app/assets/javascripts/greeting.js
@@ -6,11 +6,11 @@ $(document).on('ready', function(){
 
     setInterval(function(){
       //console.log('C:'++curr);
-      $('#greetings .hello').hide("slide", {direction: "up"}, 500);
-      $('#greetings .hello:nth-child('+curr+')').delay(500).show("slide", {direction: "down"}, 500);
+      $('#greetings .hello').hide();
+      $('#greetings .hello:nth-child('+curr+')').delay(100).show("slide", {direction: "down"}, 500);
       curr++;
       if (curr == count+1){
-        $('#greetings .hello:nth-child(9)').delay(3000).hide("slide", {direction: "up"}, 500);
+        $('#greetings .hello:nth-child(9)').delay(500).hide();
         curr = 1;
       }
     }, 5000);
