20f30119d7a6923410694b9593b55779d1f84de8
Erin Kim <kim.erin.kim@gmail.com>
2016-07-15 15:24:04 -0400

    fix intro to flex box

--- app/assets/javascripts/square.js
+++ app/assets/javascripts/square.js
@@ -1,17 +1,17 @@
-$(window).on('load', function(){
-
-var square = new Sonic({
-    width: 100,
-    height: 100,
-    fillColor: '#000',
-    path: [
-        ['line', 10, 10, 90, 10],
-        ['line', 90, 10, 90, 90],
-        ['line', 90, 90, 10, 90],
-        ['line', 10, 90, 10, 10]
-    ]
-});
-square.play();
-// document.body.appendChild(square.canvas);
-
-});
+// $(window).on('load', function(){
+//
+// var square = new Sonic({
+//     width: 100,
+//     height: 100,
+//     fillColor: '#000',
+//     path: [
+//         ['line', 10, 10, 90, 10],
+//         ['line', 90, 10, 90, 90],
+//         ['line', 90, 90, 10, 90],
+//         ['line', 10, 90, 10, 10]
+//     ]
+// });
+// square.play();
+// // document.body.appendChild(square.canvas);
+//
+// });
